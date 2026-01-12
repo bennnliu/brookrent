f9adffe562d9b7364d35ccef506f6de0aadc857b
bennn <bennnliu8067@gmail.com>
2026-01-11 23:45:40 -0500

    Prod Fix 6

--- backend/middleware/arcjetMiddleware.js
+++ backend/middleware/arcjetMiddleware.js
@@ -21,11 +21,7 @@ const aj = arcjet({
 
 const ajDecision = async (req, res, next) => {
     try {
-        console.log("Allow Proxy:", app.get('trust proxy'));
-        console.log("Client IP:", req.ip); 
-        console.log("X-Forwarded-For:", req.headers['x-forwarded-for']);
         const decision = await aj.protect(req, { requested: 1 });
-        console.log("Arcjet decision", decision);
 
         if (decision.isDenied()) {
             if (decision.reason.isRateLimit()) {
