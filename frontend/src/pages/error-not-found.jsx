49f744af468bb567d8ce825749f741953bd3b110
yenquang000 <yenq98662@gmail.com>
2026-01-11 19:13:21 -0500

    404 page updated

--- frontend/src/pages/error-not-found.jsx
+++ frontend/src/pages/error-not-found.jsx
@@ -1,13 +1,31 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import NavBar from "../components/navbar.jsx";
+import Logo from "@/assets/logoWITHOUTname.png";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
-//Will be implemented. Just using current version as a placeholder
-const ErrorNotFound = () => {
+const ErrorNotFound = ({ user, setUser, loading }) => {
   return (
-    <div>
-        <h1>404 Error Not Found</h1>
-        <Link to="/">Go back to homepage</Link>
-    </div>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <NavBar user={user} setUser={setUser} loading={loading} />
 
-export default ErrorNotFound
+      <main className="flex-grow flex flex-col items-center justify-center text-center px-4 mt-24">
+        <h1 className="text-5xl font-bold mb-4 text-gray-900">
+          Oops! The page you were looking for doesn't exist
+        </h1>
+        <p className="text-gray-600 mb-8 text-lg">
+          You may have mistyped the address or the page may have moved.
+        </p>
+        <Button
+          asChild
+          size="md"
+          className="bg-[rgb(153,0,0)] hover:bg-[rgb(120,0,0)] text-white transition-colors"
+        >
+          <Link to="/">Go back to homepage</Link>
+        </Button>
+      </main>
+    </div>
+  );
+};
+
+export default ErrorNotFound;
