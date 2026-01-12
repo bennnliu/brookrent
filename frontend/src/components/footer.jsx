56a8579b8d31a45417165af35f60099cd9ee6ad5
bennn <bennnliu8067@gmail.com>
2026-01-12 00:34:33 -0500

    Prod Fix 12

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -51,12 +51,12 @@ export default function FooterSection() {
           </a>
 
           <a
-            href="/contact"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="TikTok"
+            aria-label="Gmail"
             className="text-muted-foreground hover:text-primary block"
           >
+            <Link to="/contact">         
             <svg
               className="size-6"
               xmlns="http://www.w3.org/2000/svg"
@@ -65,7 +65,8 @@ export default function FooterSection() {
               viewBox="0 0 24 24"
             >
              <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z " fill="currentColor"/>
-            </svg>
+            </svg></Link>
+   
           </a>
         </div>
 
